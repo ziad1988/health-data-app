dc00934dc28e3750e68a1fb8c32c1466c40fbdb7
Ziad Nader <ziad.nader88@gmail.com>
2021-05-16 20:51:57 +0200

    change data file

--- Health_Dashboard.py
+++ Health_Dashboard.py
@@ -42,7 +42,7 @@ last_year = datetime.date(2020, 3, 16)
 
 @st.cache
 def load_data():
-    workout_df = pd.read_json('/Users/ziadNader/Desktop/Personal Projects/Apple Health /streamlit-app/workouts_processed.json')
+    workout_df = pd.read_json('workouts_processed.json')
     return workout_df
 
 workout_df = load_data()
